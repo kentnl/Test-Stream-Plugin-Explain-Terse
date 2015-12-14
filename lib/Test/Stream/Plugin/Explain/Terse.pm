8e3fc70f18200d7e49f7a481c62380b327eb324f
Kent Fredric <kentfredric@gmail.com>
2015-12-15 00:50:14 +1300

    v0.001001
     - Import ::Core explicitly stating the need for cmp_ok, which was omitted from default_exports before Test::Stream
       1.302025-TRIAL
     - Require at least 1.302025, which is the first version that had a cmp_ok at all.
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001000, see misc/*.deps* for details
     - test: +2 ↑1

--- lib/Test/Stream/Plugin/Explain/Terse.pm
+++ lib/Test/Stream/Plugin/Explain/Terse.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Test::Stream::Plugin::Explain::Terse;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Dump anything in a single line in 80 characters or fewer
 
